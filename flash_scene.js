2c46f321aa1afc14156c2451ccedb4908f25800d
Poetics <87587068+ThePoetics@users.noreply.github.com>
2026-04-21 14:45:49 -0700

    Create flash_scene.js

---
+++ flash_scene.js
@@ -0,0 +1,10 @@
+// This script creates a light that will illuminate the scene for everyone *in the scene* with "Provides Vision" and then the light will 
+// vanish nearly instantly leaving the map 'explored' but monsters hidden until they're in line of sight.  If you have any players not 
+// in the scene, they will not get the benefit of this 'exploration'.
+// Credit to mxzf on the Foundry Discord
+
+const dimensions = canvas.scene.dimensions
+let [created_light] = await canvas.scene.createEmbeddedDocuments('AmbientLight', [{'config.dim': dimensions.maxR, vision: true, walls: false, x: dimensions.width/2, y: dimensions.height/2}])
+await new Promise(r => setTimeout(r, 100));
+await created_light.update({hidden: true})
+await created_light.delete()
